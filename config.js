e26d199b237bff1396508ea9372d61468f6d0eb2
Mladenoliki <96631126+Mladenoliki@users.noreply.github.com>
2021-12-26 12:58:08 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.TOKEN = 'yourtoken';
+
+exports.PREFIX = '.';
+
+exports.news_API = 'a4257f13fdce4c1f895d922aa9ed6e98';
+
+exports.giphy_API = 'J6uSs68pupRQEfPHFXq6G9ggLQtOakQr';
+
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6eS7ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
